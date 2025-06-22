dbc6481c226b96745c02abc061fbab6f425e658f
ichwalM <13020230049@student.umi.ac.id>
2025-06-22 23:55:29 +0800

    finishing header_1.4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,9 @@ const Header = ({ activeTab, setActiveTab }) => {
         <>
             <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between p-4 mx-auto">
-                    <Logo />
+                    <figure onClick={()=>handleNavLinkClick('home')}>
+                        <Logo />
+                    </figure>
                     <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                         <button
                             onClick={handleMenuToggle}
