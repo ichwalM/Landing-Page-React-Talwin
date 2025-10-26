9a4bdd1db4bee5ca352ae8681d20f8e90c829708
ichwalM <13020230049@student.umi.ac.id>
2025-10-26 17:26:34 +0800

    fix-error path production

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
   base: '/ichwal/',
+  plugins: [react(), tailwindcss(), flowbiteReact()],
   build: {
     outDir: 'dist',
   },
