9a89eff52836eab7533dbac50af032d1aede5e3a
ichwalM <13020230049@student.umi.ac.id>
2025-10-28 16:57:14 +0800

    test production

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  
   base: '/ichwal/',
   plugins: [react(), tailwindcss(), flowbiteReact()],
 })
